c05406dbdc5ca66a07dd6031caf1d78e6aee5fcd
Fernanda Alcala Durand <fleoren@users.noreply.github.com>
2016-04-29 22:45:19 -0500

    new ideas for learning process
    * How to generate possible policies
    * Extra constraint to not leave the grid
    * The random length of the policy forces the agent to learn where the goal is, this is not a priori information

--- draft.py
+++ draft.py
@@ -4,7 +4,9 @@ import numpy
 
 ########### WIND
 # 7x10 matrix
-wind_matrix <- numpy.zeros((7,10))
+width = 7
+length = 10
+wind_matrix <- numpy.zeros((width,length))
 
 #values for each one of the columns
 wind_values = (0,0,0,1,1,1,2,2,1,0)
@@ -19,7 +21,7 @@ for i in range(len(wind_values)):
 
 #start 3,0
 
-########## AGENT POSSIBLE ACTIONS ##########
+########## DEFINING ACTORS AND ENVIRONMENT, AND HOW THEY INTERACT ##########
 
 class Square:
 	def __init__(self,x,y):
@@ -31,10 +33,31 @@ class Square:
 
 def move_agent(from_sq,to_sq):
 	#if it tries to leave the grid, it stays on the square where it was
-	if( to_sq.x < 0 or to_sq.x > 6 or to_sq.y < 0 or to_sq > 9 ):
+	if( (to_sq.x < 0) or (to_sq.x > width - 1) or (to_sq.y < 0) or (to_sq > length - 1) ):
 		moves_to = from_sq
-	#else, we sum the value of wind to the y coordinate	
-	else( to_sq.y = to_sq.y + to_sq.wind )
+	#wind can't take it out of the grid
+	elif( to_sq.y + to_sq.wind > width - 1 ):
+		to.sq.y = width
+	#else, we sum the value of wind to the y coordinate
+	else( to_sq.y = to_sq.y + to_sq.wind ):
 		moves_to = to_sq
 
-	return moves_to		
+	return moves_to	
+
+########## AGENT POSSIBLE ACTIONS ##########
+	
+# agent should move in any of 4 directions: N, E, S, W
+
+# for 1:100000
+	# ideas para quitar el for:
+		# cuando el best_policy no cambie en *N iteraciones, pum
+
+# TODO simulate one policy
+# randint [1,width*length] to obtain the length of the policy
+# fill it out with random choices from the 4 possible
+# evaluate policy
+
+# first time: save policy as best_policy
+	# if reward(current_policy) > reward(best_policy) then best_policy = current_policy
+
+
