cc816ba4b84e05ae22b19f4efd154b7c50822771
Fernanda Alcala Durand <fleoren@users.noreply.github.com>
2016-10-26 18:39:10 -0500

    Basic customer and fields behavior
    Code snippets that create both the input `csv` files for the main code and illustrative plots

---
+++ Beerhouse/code_drafts/demand_plots.R
@@ -0,0 +1,85 @@
+# This script creates the plots to illustrate the yearly demand trends
+# It also munches the "visual friendly" yearly demand to a decent csv file
+# that can be read directly as demand trend
+
+wd <- "" ## where to find the
+      ## excel files that contain demand trends for consumer
+setwd(wd)
+
+library(readxl)
+library(ggplot2)
+
+# Reading files ##############################################################
+demands_no_noise <- read_excel("creating_consumer_trend.xlsx", sheet = 5,
+                                 skip = 1)
+
+days_of_every_month <- read_excel("creating_consumer_trend.xlsx", sheet = 2,
+                                 skip = 0)
+
+# Cleaning data ##############################################################
+# Demands data frame ###############
+# Some numbers come with a lot of decimal places because of number format
+is.num <- sapply(demands_no_noise, is.numeric)
+demands_no_noise[is.num] <- lapply(demands_no_noise[is.num], round, 2)
+demands_no_noise <- as.data.frame(t(demands_no_noise),stringsAsFactors=FALSE)
+
+# Managing the data frame. We want to end up with a month column, a day 
+#   column and a demand column.
+
+# Create an empty data frame named as the month
+get_daily_demands <- function(row){
+    df <- data.frame(Month = character(days_of_every_month$Days[row]),
+                  Day = character(days_of_every_month$Days[row]),
+                  Demand = numeric(days_of_every_month$Days[row]),
+                  stringsAsFactors=FALSE)
+    
+    df$Month <- days_of_every_month$Month[row]
+    df$Day <- 1:days_of_every_month$Days[row]
+    df$Demand <- as.numeric(na.omit(as.vector(t(demands_no_noise[2:nrow(demands_no_noise),row]))))
+    df
+}
+
+all_months_demands <- list()
+
+# Create list of dataframes, 12 one for each month
+for(i in 1:nrow(days_of_every_month)){
+  all_months_demands[[i]] <- get_daily_demands(i)
+}
+
+# Get only one big dataframe
+demands_no_noise <- do.call("rbind", all_months_demands)
+# Clean up Month as factors to get the correct order
+demands_no_noise$Month <- factor(demands_no_noise$Month,
+                                 levels = c("January", "February", "March",
+                                            "April", "May", "June", "July",
+                                            "August", "September", "October",
+                                            "November", "December" ))
+
+# Plots ######################################################################
+
+
+all_months <- ggplot(demands_no_noise, aes(x = Day, y = Demand, color = Demand)) + 
+                          geom_line(size=1) +
+                          facet_wrap( ~ Month) +
+                          guides(color = "none") +
+                          scale_colour_continuous(low = "darkgrey", high = "firebrick4") +
+                          theme(panel.background = element_rect(fill = "gray95")) + 
+                          labs(x = "Day", y = "Demand") +
+                          ggtitle("Yearly Beer Demand")
+all_months
+
+example_month <- ggplot(subset(demands_no_noise, Month == "August"), aes(x = Day, y = Demand, color = Demand)) + 
+                          geom_line(size=1) +
+                          facet_wrap( ~ Month) +
+                          guides(color = "none") +
+                          scale_colour_continuous(low = "darkgrey", high = "firebrick4") +
+                          theme(panel.background = element_rect(fill = "gray95")) +
+                          scale_y_continuous(limits = c(0.5, 2)) + 
+                          labs(x = "Day", y = "Demand") +
+                          ggtitle("Example Normal Month")
+example_month
+
+# Anything below this line is not part of the code, erase if present
+
+write.csv(demands_no_noise, "customer_trend.csv", row.names = FALSE)
+
---
+++ Beerhouse/code_drafts/supply_plots.R
@@ -0,0 +1,81 @@
+# This Script takes the consolidated table with harvest seasons
+#"1. Creates a decent csv that can be read directly as fields trend
+
+wd <- "" ## where to find the
+## excel files that contain demand trends for consumer
+setwd(wd)
+
+library(readxl)
+library(ggplot2)
+library(dplyr)
+
+# Reading files ##############################################################
+fields_no_noise <- read_excel("creating_fields_trend.xlsx", sheet = 2,
+                               skip = 0)
+all_harvests <- list()
+
+# This function creates a vector 
+get_harvest<- function(row){
+  harvest_dates <- seq(as.Date(fields_no_noise$begin_date[row]), 
+                      as.Date(fields_no_noise$end_date[row]), "days")
+  active_dates <- seq(as.Date(fields_no_noise$begin_date_active[row]), 
+                       as.Date(fields_no_noise$end_date_active[row]), "days")
+  
+  # We're gonna assume that an "active" day yields twice as a regular day.
+  # So baseline: 1 "unit" every regular day
+  regular_harvest <- data.frame(Date = harvest_dates,
+                                basic_harvest = rep(1, length(harvest_dates)))
+  active_harvest <- data.frame(Date = active_dates,
+                               active_harvest = rep(1, length(active_dates)))
+  
+  harvest <- regular_harvest %>% left_join(active_harvest)  %>% 
+    mutate(active_harvest= ifelse(is.na(active_harvest), 0, active_harvest),
+           total_harvest = basic_harvest + active_harvest,
+           harvest_proportion = total_harvest/sum(total_harvest),
+           harvest = harvest_proportion*fields_no_noise$harvest[row]) %>%
+           select(Date,harvest) 
+}
+
+for(i in 1:nrow(fields_no_noise)){
+all_harvests[[i]] <- get_harvest(i)
+}
+
+
+fields_no_noise <- do.call("rbind", all_harvests) %>%
+  filter(is.na(harvest) == FALSE) %>%
+  group_by(Date) %>%
+  summarise(total_harvest = sum(harvest)) %>%
+  ungroup
+
+# csv should have Month, Day, Supply fields
+
+one_year <- data.frame(Date = seq(as.Date('2013-01-01'),as.Date('2013-12-31'), "days"))
+
+fields_no_noise_for_csv <- one_year %>% left_join(fields_no_noise) %>%
+  mutate(Supply= as.numeric(ifelse(is.na(total_harvest), 0, total_harvest)),
+         Month = substr(Date, 6,7),
+         Month = factor(Month,
+            levels = c("01", "02", "03", "04", "05", "06",
+                       "07", "08", "09", "10", "11", "12"),
+            labels = c("January", "February", "March",
+                       "April", "May", "June", "July",
+                       "August", "September", "October",
+                       "November", "December" )),
+         Month = as.character(Month),
+         Day = substr(Date, 9,10)) %>%
+  select(Month, Day, Supply)
+
+# Plot!
+q <- ggplot(fields_no_noise, aes(x = Date, y = total_harvest, color = total_harvest)) + 
+  geom_line()  +
+  guides(color = "none") +
+  scale_colour_continuous(low = "darkgrey", high = "firebrick4") +
+  theme(panel.background = element_rect(fill = "gray95")) + 
+  labs(x = "Month", y = "Harvest") +
+  ggtitle("Yearly Barley Harvest")
+q
+
+# Anything below this line is not part of the code, erase if present
+
+write.csv(fields_no_noise_for_csv, "fields_trend.csv", row.names = FALSE)
+
